06eb92b8278564afdefc2cd8352ed3e3c70ee025
ildc21 <32519163+ildc21@users.noreply.github.com>
2018-03-26 11:30:11 -0400

    Add files via upload

---
+++ Quiz_Week_9/Quiz_Week_9a.ino
@@ -0,0 +1,38 @@
+// create an array to store the digital pin
+// info for 5 LEDs
+int ledPin[] = {4, 5, 6, 7};
+ 
+ 
+void setup() {
+ 
+  // enable serial (like a console)
+  Serial.begin(9600);
+  // hello world! print out serial
+  Serial.println("Serial Ready");
+ 
+  // generate a new random seed
+  randomSeed(analogRead(0));
+ 
+  // ...
+  int rando = random(4);
+  Serial.println(rando);
+ 
+  // sets all LED pins to OUTPUT
+  for(int i = 2; i < 4; i++) {
+    pinMode(ledPin[i], OUTPUT);
+  }
+ 
+  // turn them all off
+  for(int i = 2; i < 4; i++) {
+    digitalWrite(ledPin[i], LOW);
+  }
+    for(int i = 2; i < rando; i++) {
+    digitalWrite(ledPin[i], HIGH);
+  }
+}
+ 
+void loop() {
+ 
+ 
+ 
+}
---
+++ Quiz_Week_9/Quiz_Week_9b.ino
@@ -0,0 +1,22 @@
+ int ledPin = 4;
+void setup() {
+ Serial.begin(9600);
+ pinMode(ledPin, INPUT);
+ 
+
+}
+
+void loop() {
+
+  int x = digitalRead(ledPin);
+  Serial.println("The led has blinked");
+  Serial.println(x);
+  delay(1);
+ 
+  digitalWrite(ledPin, HIGH);
+  delay (400);
+  digitalWrite(ledPin, LOW);
+  delay(400);
+ 
+
+}
